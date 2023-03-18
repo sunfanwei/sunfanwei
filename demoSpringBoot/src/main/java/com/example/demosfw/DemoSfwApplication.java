93fb1d7d39f093a9dabb8dacafd75d8b7da928d7
sunfanwei <1393190762@qq.com>
2023-03-18 19:11:52 +0800

    spring move

--- demoSpringBoot/src/main/java/com/example/demosfw/DemoSfwApplication.java
+++ demoSpringBoot/src/main/java/com/example/demosfw/DemoSfwApplication.java
@@ -1,4 +1,4 @@
-package com.example.demosfw.Aop;
+package com.example.demosfw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
