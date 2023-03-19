ac65833b92b1f2eda5e74bf567081a0e57886cb8
sunfanwei <1393190762@qq.com>
2023-03-19 09:23:03 +0800

    spring

--- demoSpringBoot/src/main/java/com/example/demosfw/Config/WebConfig.java
+++ demoSpringBoot/src/main/java/com/example/demosfw/Config/WebConfig.java
@@ -1,8 +1,12 @@
 package com.example.demosfw.Config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.HashSet;
@@ -10,6 +14,20 @@ import java.util.Set;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/zxc/foo").setViewName("foo");
+        registry.addViewController("/").setViewName("forward:login123.jsp");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor())
+                .addPathPatterns("/asd/**");
+    }
+
     @Bean
     public FilterRegistrationBean TimeFilter(){
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
@@ -25,6 +43,22 @@ public class WebConfig implements WebMvcConfigurer {
         // 设置过滤器的执行顺序
         filterRegistrationBean.setOrder(1);
         return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegist() {
+        FilterRegistrationBean frBean = new FilterRegistrationBean();
+        frBean.setFilter(new MyFilter());
+        frBean.addUrlPatterns("/*");
+        System.out.println("filter");
+        return frBean;
+    }
+    @Bean
+    public ServletListenerRegistrationBean listenerRegist() {
+        ServletListenerRegistrationBean srb = new ServletListenerRegistrationBean();
+        srb.setListener(new MyHttpSessionListener());
+        System.out.println("listener");
+        return srb;
     }
 }
 
