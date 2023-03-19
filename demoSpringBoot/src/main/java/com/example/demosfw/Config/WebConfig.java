50c1ff384e37d1eac36d739f46aa5cb34587103e
sunfanwei <1393190762@qq.com>
2023-03-19 11:34:20 +0800

    spring

--- demoSpringBoot/src/main/java/com/example/demosfw/Config/WebConfig.java
+++ demoSpringBoot/src/main/java/com/example/demosfw/Config/WebConfig.java
@@ -14,35 +14,10 @@ import java.util.Set;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/zxc/foo").setViewName("foo");
-        registry.addViewController("/").setViewName("forward:login123.jsp");
-        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-
-    }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new MyInterceptor())
                 .addPathPatterns("/asd/**");
-    }
-
-    @Bean
-    public FilterRegistrationBean TimeFilter(){
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
-        TimeFilter timeFilter = new TimeFilter();
-        filterRegistrationBean.setFilter(timeFilter);
-        // 添加拦截路径
-        filterRegistrationBean.addUrlPatterns("/filter/*");
-        Set<String> path = new HashSet<>();
-        // 多路径情况
-        // path.add("/filter/test1");
-        // path.add("/filter/test2");
-        // filterRegistrationBean.setUrlPatterns(path);
-        // 设置过滤器的执行顺序
-        filterRegistrationBean.setOrder(1);
-        return filterRegistrationBean;
     }
 
     @Bean
